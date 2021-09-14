68b46209c32f2fd2722a2708f0bba3d9bb87906f
nurhesen <33550425+nurhesen@users.noreply.github.com>
2021-09-14 18:42:56 +0400

    Add files via upload

--- createuser.py
+++ createuser.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import User
+from api.models import Author
+from api.models import NewsModel
 try:
-    User.objects.create_superuser('test', 'test@example.com', 'test')
+    usr=User.objects.create_superuser('test', 'test@example.com', 'test')
+    authr=Author.objects.create(name="Authorname", last_name="Authorlastname", user=usr)
+    news=NewsModel.objects.create(news_title="Title of new news", news_content="Content of news. It can be as long as you want", news_author=authr)
+
 except:
     pass
