45648374881dbef9493f34db065802bbc55a1ee9
nurhesen <33550425+nurhesen@users.noreply.github.com>
2021-11-30 19:58:13 +0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
+        'DISABLE_SERVER_SIDE_CURSORS': True,
+
     }
 }
 
